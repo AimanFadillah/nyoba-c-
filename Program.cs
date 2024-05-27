54b4f43240a48c5f028d7db3a5d2c5ab3c159725
Aiman Fadillah <mlakece914@gmail.com>
2024-05-27 10:10:25 +0700

    Add const,multiple variable,casting and user input

--- Program.cs
+++ Program.cs
@@ -6,11 +6,33 @@ namespace HelloWord{
             Console.WriteLine("Hello Word"); // Console.log di C#
 
             // Variabel
-            int angka = 1;
-            double angka2 = 19.22;
-            char huruf = 'A';
-            bool type = true;
-            string kalimat = "Hello";
+                int angka = 1;
+                double angka2 = 19.22;
+                char huruf = 'A';
+                bool type = true;
+                string kalimat = "Hello";
+
+            // Const (Tidak boleh dirubah)
+                const int angka3 = 1;
+
+            // Multiple Variable
+                int int1 = 1,int2 = 2,int3 = 3;
+
+            // Casting
+                int myInt = 10;
+                double myDouble = 9.78;
+                bool myBool = true;
+
+                Convert.ToString(myInt);
+                Convert.ToDouble(myInt);
+                Convert.ToInt32(myDouble); // to Int
+                Convert.ToString(myBool);
+            
+            // User Input
+                Console.WriteLine("Enter username:");
+                string username = Console.ReadLine() ?? "Kosong";
+                Console.WriteLine("Your name " + username);
+
         }
     }
 }
