553b730debb302765875967954f71998c6a86e8d
Aiman Fadillah <mlakece914@gmail.com>
2024-05-27 09:52:03 +0700

    variable

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWord{
+    class Program{
+        static void Main (String[] args){
+            Console.WriteLine("Hello Word"); // Console.log di C#
+
+            // Variabel
+            int angka = 1;
+            double angka2 = 19.22;
+            char huruf = 'A';
+            bool type = true;
+            string kalimat = "Hello";
+        }
+    }
+}
