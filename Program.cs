5c1c2c9f9881bf72acb90b770f9ca7e15c202c62
Aiman Fadillah <mlakece914@gmail.com>
2024-05-27 10:28:03 +0700

    add math,strings,interpolation and acces strings

--- Program.cs
+++ Program.cs
@@ -33,6 +33,29 @@ namespace HelloWord{
                 string username = Console.ReadLine() ?? "Kosong";
                 Console.WriteLine("Your name " + username);
 
+            // Math
+                Math.Max(5,10); // 10
+                Math.Min(5,10); // 5
+                Math.Sqrt(64);  // 8
+                Math.Abs(-4.7); // Always positif
+                Math.Round(9.99); // 10
+            
+            // Strings
+                string txt = "Hai";
+                txt.ToUpper() ; // HAI
+                txt.ToLower() ; // hai
+
+            // Interpolation
+                string firstname = "Jhon";
+                string lastname = "Doe";
+                string name = $"My full name is {firstname} {lastname}";
+            
+            // Access Strings
+                string myString = "Hello Word";
+                Console.WriteLine(myString[0]); // "H"
+                Console.WriteLine(myString[1]); // "e"
+                Console.WriteLine(myString.IndexOf("e")); // 1
+                Console.WriteLine(myString.Substring(7)); // Word
         }
     }
 }
